9ee29ac78b715403b46caa3a399d0c5b4c5260f0
Moses <rlakim@ucdavis.edu>
2016-02-17 19:25:10 -0800

    starting

--- Event.py
+++ Event.py
@@ -1,3 +1,15 @@
 
+"""***
+	Event.py
+***"""
+import math
+import random
 
-float time = 0;
+time = 0;
+
+def nega_exp_dist_time(rate):
+     u = random.uniform(0,1);
+     return ((-1/rate)*math.log(1-u));
+
+
+class wow:
